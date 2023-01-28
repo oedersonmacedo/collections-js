225b730edacfaf34a91db5f604cf51d333ffcb47
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 09:43:44 -0300

    Example for accesor array, with includes

---
+++ array/accessor-api/example-2.js
@@ -0,0 +1,10 @@
+const teams = ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United'];
+
+console.log(teams.includes("Criciuma"));
+// expect output: true
+
+console.log(teams.includes("Vasco"));
+// expect output: true
+
+console.log(teams.includes("Palmeiras"));
+// expect output: false
