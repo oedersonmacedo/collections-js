7f5c9e3abb2113250840e69b7efbd564bdebcd09
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 07:02:17 -0300

    Example of how assigns in array by position

---
+++ array/example-3.js
@@ -0,0 +1,11 @@
+const teams = [];
+teams[0] = 'Vasco';
+teams[1] = 'Criciuma';
+teams[2] = 'Real Madrid';
+teams[3] = 'Manchester United';
+
+console.log(teams);
+// expect output: ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United']
+
+console.log(teams.length);
+// expect output: 4
