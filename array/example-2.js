f08c47190d58c20e2212d71309b4c66d56f96165
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 06:59:56 -0300

    Example of how assigns in array

---
+++ array/example-2.js
@@ -0,0 +1,8 @@
+const teams1 = ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United'];
+const teams2 = new Array('Vasco', 'Criciuma', 'Real Madrid', 'Manchester United');
+
+console.log(teams1);
+// expect output: ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United']
+
+console.log(teams2);
+// expect output: ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United']
