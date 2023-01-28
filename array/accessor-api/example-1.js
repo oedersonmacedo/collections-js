39db804ec8fe9f6da36637ed6aa8e84d25d6e380
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 09:43:12 -0300

    Example for accesor array, with lastIndexOf and indexOf

---
+++ array/accessor-api/example-1.js
@@ -0,0 +1,10 @@
+const teams = ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United'];
+
+console.log(teams.indexOf("Criciuma"));
+// expect output: 1
+
+console.log(teams.lastIndexOf("Vasco"));
+// expect output: 0
+
+console.log(teams.indexOf("Palmeiras"));
+// expect output: -1
