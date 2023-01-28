6f655f9eccc28b8e884616e762f770d3704a262e
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 09:44:49 -0300

    Example for accesor array, with slice values

---
+++ array/accessor-api/example-4.js
@@ -0,0 +1,10 @@
+const teams = ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United', 'Atletico-MG', 'Palmeiras' ];
+
+console.log(teams.slice(0, 2));
+// expect output: [ 'Vasco', 'Criciuma' ]
+
+console.log(teams.slice(2, 4));
+// expect output: [ 'Real Madrid', 'Manchester United' ]
+
+console.log(teams.slice(1));
+// expect output: [ 'Criciuma', 'Real Madrid', 'Manchester United', 'Atletico-MG', 'Palmeiras' ]
