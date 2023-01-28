0a01fc1287b375cfd065654aea6c17e81ed73cd8
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 07:05:30 -0300

    Example by position but set position 0,10,20,30

---
+++ array/example-5.js
@@ -0,0 +1,11 @@
+const teams = [];
+teams[0] = 'Vasco';
+teams[10] = 'Criciuma';
+teams[20] = 'Real Madrid';
+teams[30] = 'Manchester United';
+
+console.log(teams);
+// expect output: [ 'Vasco', <9 empty items>, 'Criciuma', <9 empty items>, 'Real Madrid', <9 empty items>, 'Manchester United' ]
+
+console.log(teams.length);
+// expect output: 31
