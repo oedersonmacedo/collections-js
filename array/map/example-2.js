fa07e967a5f5426e975468c468300c81bef91ca9
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 15:26:26 -0300

    Example about set Map through the function

---
+++ array/map/example-2.js
@@ -0,0 +1,7 @@
+const timeUnits = new Map();
+timeUnits.set("second", 1);
+timeUnits.set("minute", 60);
+timeUnits.set("hour", 3600);
+
+console.log(timeUnits);
+// expect output: Map(3) { 'second' => 1, 'minute' => 60, 'hour' => 3600 }
