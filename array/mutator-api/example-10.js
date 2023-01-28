95dfda4eb05297b8a2d316af276f6b8b6b5907f8
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 08:33:42 -0300

    Example for mutation of array, with fill

---
+++ array/mutator-api/example-10.js
@@ -0,0 +1,5 @@
+const teams = ['Palmeiras', 'Criciuma', 'Real Madrid', 'Manchester United'];
+
+teams.fill('Vasco', 1);
+console.log(teams)
+// expect output: [ 'Palmeiras', 'Vasco', 'Vasco', 'Vasco' ]
---
+++ array/mutator-api/example-11.js
@@ -0,0 +1,6 @@
+const teams = ['Palemiras', 'Criciuma', 'Real Madrid', 'Manchester United'];
+
+teams.fill("Vasco", 0, 2);
+
+console.log(teams);
+// expect output: [ 'Vasco', 'Vasco', 'Real Madrid', 'Manchester United' ]
