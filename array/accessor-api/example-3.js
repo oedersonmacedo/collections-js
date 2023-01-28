8132930a9cbb2954c70b1f070285ab5b2c6195f8
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 09:44:17 -0300

    Example for accesor array, with concat arrays

---
+++ array/accessor-api/example-3.js
@@ -0,0 +1,11 @@
+const teams = ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United'];
+const otherTeams = [ 'Atletico-MG', 'Palmeiras' ];
+
+console.log(teams.concat(otherTeams));
+// expect output: [ 'Vasco', 'Criciuma', 'Real Madrid', 'Manchester United', 'Atletico-MG', 'Palmeiras' ]
+
+console.log(teams);
+// expect output: [ 'Vasco', 'Criciuma', 'Real Madrid', 'Manchester United' ]
+
+console.log(otherTeams);
+// expect output: [  'Atletico-MG', 'Palmeiras' ]
