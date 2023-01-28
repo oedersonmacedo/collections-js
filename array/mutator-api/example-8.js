dbe65afceb8624b406aed658218a56e4b9018b43
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 08:32:46 -0300

    Example for mutation of array, with reverse values

---
+++ array/mutator-api/example-8.js
@@ -0,0 +1,9 @@
+const teams = ['Vasco', 'Criciuma', 'Real Madrid', 'Manchester United'];
+
+
+teams.reverse();
+console.log(teams);
+// expect output: [ 'Manchester United', 'Real Madrid', 'Criciuma', 'Vasco' ]
+teams.reverse();
+console.log(teams);
+// expect output: [ 'Vasco', 'Criciuma', 'Real Madrid', 'Manchester United' ]
