837a15ef10f300bf8e01c94bfd2d2d637d3e68aa
Ederson Macedo <ederson.mos@gmail.com>
2023-01-28 09:18:25 -0300

    Example for iteration array, manipulated with some

---
+++ array/iteration-api/example-4.js
@@ -0,0 +1,25 @@
+const teams = [
+    {
+        name: 'Vasco',
+        year: 1898
+    },
+    {
+        name: 'Criciuma',
+        year: 1947
+    },
+    {
+        name: 'Real Madrid',
+        year: 1902
+    },
+    {
+        name: 'Manchester United',
+        year: 1878
+    }
+];
+
+const result = teams.some(function (team) {
+    return team.year < 1900;
+});
+
+console.log(result);
+// expect output: true
