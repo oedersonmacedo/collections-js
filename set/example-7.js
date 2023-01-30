13079f9d4238cb226e000383cc8b8a41ae60313b
Ederson Macedo <ederson.mos@gmail.com>
2023-01-30 07:56:38 -0300

    Example about the Set, passed Array for Set

---
+++ set/example-7.js
@@ -0,0 +1,13 @@
+const array = [10, 10, 10];
+console.log(array);
+// expect output: [ 10, 10, 10 ]
+
+console.log(array.length);
+// expect output: 3
+
+const set = new Set(array);
+console.log(set);
+// expect output: Set(1) { '10' }
+
+console.log(set.size);
+// expect output: 1
