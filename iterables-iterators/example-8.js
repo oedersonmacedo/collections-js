8ca9d1bcb410f5e05120d56f80c7d4536a9ff0eb
Ederson Macedo <ederson.mos@gmail.com>
2023-02-01 15:16:11 -0300

    Example iterator in Map

---
+++ iterables-iterators/example-8.js
@@ -0,0 +1,13 @@
+const languages = new Map([["Fortran", 1957], ["Lisp", 1958], ["COBOL", 1959]]);
+const iterator = languages.entries();
+console.log(iterator.next());
+// expect output: { value: [ 'Fortran', 1957 ], done: false }
+
+console.log(iterator.next());
+// expect output: { value: [ 'Lisp', 1958 ], done: false }
+
+console.log(iterator.next());
+// expect output: { value: [ 'COBOL', 1959 ], done: false }
+
+console.log(iterator.next());
+// expect output: { value: undefined, done: true }
