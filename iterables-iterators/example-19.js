1ce9715cef3e9c48dd3f4849ce1d28426a32a338
Ederson Macedo <ederson.mos@gmail.com>
2023-02-01 15:26:17 -0300

    Example function create iterator for loop generate Error

---
+++ iterables-iterators/example-19.js
@@ -0,0 +1,24 @@
+function createIterator(...array) {
+    let i = 0;
+    return {
+        next() {
+            if (i < array.length) {
+                return {
+                    value: array[i++],
+                    done: false
+                }
+            } else {
+                return {
+                    value: undefined,
+                    done: true
+                }
+            }
+        }
+    }
+}
+const iterator = createIterator("Fortran", "Lisp", "COBOL");
+for (let language of iterator) {
+    console.log(language);
+}
+
+//generate error: TypeError: iterator is not iterable
