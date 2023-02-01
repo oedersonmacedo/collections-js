cf73c461de0dc7df18134ab5ee884ce7cf38377b
Ederson Macedo <ederson.mos@gmail.com>
2023-02-01 15:15:25 -0300

    Example iterator in Array

---
+++ iterables-iterators/example-6.js
@@ -0,0 +1,14 @@
+const languages = ["Fortran", "Lisp", "COBOL"];
+const iterator = languages[Symbol.iterator]();
+console.log(iterator.next());
+// expect output: { value: 'Fortran', done: false }
+
+console.log(iterator.next());
+// expect output: { value: 'Lisp', done: false }
+
+console.log(iterator.next());
+// expect output: { value: 'COBOL', done: false }
+
+console.log(iterator.next());
+// expect output: { value: undefined, done: true }
+
